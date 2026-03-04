c77c467b3b4b38a19c3933e6601c1584fcfdaa8d
Xzonn <Xzonn@outlook.com>
2026-03-04 22:36:23 +0800

    20260303

--- consts.js
+++ consts.js
@@ -1,6 +1,6 @@
 const TranslateServerUrl = "ws://localhost:32802/ws";
 const CommentServerUrl = "ws://localhost:32804/ws";
 
-const FadeInTime = new Date("2026-02-27T21:57:30+08:00");
-const StartTime = new Date("2026-02-27T22:00:00+08:00");
-const EndTime = new Date("2026-02-27T22:25:00+08:00");
+const FadeInTime = new Date("2026-03-03T21:56:15+08:00");
+const StartTime = new Date("2026-03-03T22:00:00+08:00");
+const EndTime = new Date("2026-03-03T22:25:00+08:00");
